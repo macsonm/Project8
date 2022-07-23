c30a5306dd96c5bf3283dd0d1d5f06d068596372
macsonm <75022784+macsonm@users.noreply.github.com>
2022-07-23 14:06:11 +0300

    TODO: add constraints
    10:00

--- Project8/ViewController.swift
+++ Project8/ViewController.swift
@@ -1,17 +1,40 @@
-//
-//  ViewController.swift
-//  Project8
-//
-//  Created by Maxim M on 23.07.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    var cluesLabel:  UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UITextField!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "score 0"
+        view.addSubview(scoreLabel)
+        
+        cluesLabel = UILabel()
+        cluesLabel.translatesAutoresizingMaskIntoConstraints = false
+        cluesLabel.font = UIFont.systemFont(ofSize: 24)
+        cluesLabel.text = "CLUEs"
+        cluesLabel.numberOfLines = 0
+        view.addSubview(cluesLabel)
+        
+        NSLayoutConstraint.activate([
+            scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
 
