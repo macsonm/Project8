a12b13b510c1d8ed69f6cc7b5982b1e6a4a6a24a
macsonm <75022784+macsonm@users.noreply.github.com>
2022-07-23 13:34:49 +0300

    Initial Commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Maxim M on 23.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
